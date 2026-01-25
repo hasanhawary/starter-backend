8fba465290b90c17d15620569eb72058262f0a46
Hassan Elhawary <h.mohammad@wakeb.tech>
2026-01-25 13:30:59 +0200

    updates

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -6,15 +6,15 @@ use Illuminate\Support\Facades\Auth;
 
 abstract class BaseController
 {
-    protected string $guard;
-    protected string $userModel;
+    protected ?string $guard;
+    protected ?string $userModel;
 
     public function __construct()
     {
         $this->guard = config('auth.defaults.guard');
         Auth::shouldUse($this->guard);
 
-        $provider = config("auth.guards.{$this->guard}.providers");
+        $provider = config("auth.guards.{$this->guard}.provider");
         $this->userModel = config("auth.providers.$provider.model");
     }
 }
--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => env('AUTH_GUARD', 'api'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'sanctum',
+            'provider' => 'users',
+        ]
     ],
 
     /*
@@ -62,9 +66,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => \App\Models\User::class,
         ],
-
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
@@ -104,7 +107,7 @@ return [
     | Password Confirmation Timeout
     |--------------------------------------------------------------------------
     |
-    | Here you may define the number of seconds before a password confirmation
+    | Here you may define the amount of seconds before a password confirmation
     | window expires and users are asked to re-enter their password via the
     | confirmation screen. By default, the timeout lasts for three hours.
     |
