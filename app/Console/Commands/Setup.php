fb235b42a5fbf440fe5dbc89a6438aacc20e9e50
Hassan Elhawary <h.mohammad@wakeb.tech>
2026-02-07 17:35:51 +0200

    updates

--- app/Console/Commands/Setup.php
+++ app/Console/Commands/Setup.php
@@ -92,7 +92,7 @@ class Setup extends Command
         // Update runtime config from env
         config([
             "database.connections.{$this->defaultConnection}.host" => $this->option('db-host'),
-            "database.connections.{$this->defaultConnection}.port" => $this->option('db-host'),
+            "database.connections.{$this->defaultConnection}.port" => $this->option('db-port'),
             "database.connections.{$this->defaultConnection}.database" => $this->db,
             "database.connections.{$this->defaultConnection}.username" => $this->option('db-username'),
             "database.connections.{$this->defaultConnection}.password" => $this->option('db-password'),
