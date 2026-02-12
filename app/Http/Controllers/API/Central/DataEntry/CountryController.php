6ffa49eda01d2f008f9c6dc016d3c5b07f2f3f66
Hassan Elhawary <h.mohammad@wakeb.tech>
2026-02-12 20:40:14 +0200

    update base request

--- app/Http/Controllers/API/Central/DataEntry/CountryController.php
+++ app/Http/Controllers/API/Central/DataEntry/CountryController.php
@@ -10,6 +10,7 @@ use App\Http\Requests\Central\Global\Other\PageRequest;
 use App\Http\Resources\Central\DataEntry\CountryResource;
 use App\Models\Central\Country;
 use App\Trait\Global\HasDeleteMethods;
+use App\Trait\Global\HasToggleActiveMethods;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Pipeline\Pipeline;
 use Illuminate\Routing\Controllers\HasMiddleware;
@@ -19,7 +20,7 @@ use function __;
 
 class CountryController extends BaseController implements HasMiddleware
 {
-    use HasDeleteMethods;
+    use HasDeleteMethods, HasToggleActiveMethods;
 
     public function __construct()
     {
--- routes/central.php
+++ routes/central.php
@@ -112,10 +112,13 @@ Route::prefix('central')->group(function () {
         | Data Entry Routes
         |--------------------------------------------------------------------------
         */
-        Route::post('countries/restore', [CountryController::class, 'restore']);
-        Route::delete('countries/delete', [CountryController::class, 'destroy']);
-        Route::delete('countries/force-delete', [CountryController::class, 'forceDelete']);
-        Route::apiResource('countries', CountryController::class);
+        Route::prefix('countries')->group(function () {
+            Route::delete('force-delete', [CountryController::class, 'forceDelete']);
+            Route::delete('delete', [CountryController::class, 'destroy']);
+            Route::post('restore', [CountryController::class, 'restore']);
+            Route::put('toggle-active', [CountryController::class, 'toggleActive']);
+            Route::apiResource('/', CountryController::class)->parameters(['' => 'country'])->except(['destroy']);
+        });
 
         /*
         |--------------------------------------------------------------------------
