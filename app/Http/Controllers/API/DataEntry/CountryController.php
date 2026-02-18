4bf88b30b7fde74ac1e8614abdc46d571dc93703
Hassan Elhawary <h.mohammad@wakeb.tech>
2026-02-18 14:53:17 +0200

    update

--- app/Http/Controllers/API/DataEntry/CountryController.php
+++ app/Http/Controllers/API/DataEntry/CountryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\DataEntry;
 
 use App\Filters\Global\JsonDisplayNameFilter;
+use App\Filters\Global\JsonNameFilter;
 use App\Filters\Global\OrderByFilter;
 use App\Http\Controllers\API\BaseController;
 use App\Http\Requests\DataEntry\CountryRequest;
@@ -44,7 +45,7 @@ class CountryController extends BaseController implements HasMiddleware
     {
         $query = app(Pipeline::class)
             ->send(Country::query())
-            ->through([JsonDisplayNameFilter::class, OrderByFilter::class])
+            ->through([JsonNameFilter::class, OrderByFilter::class])
             ->thenReturn();
 
         return successResponse(wrapPaginate($query, CountryResource::class));
